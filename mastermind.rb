6afeceb16bc32a3583e496bca02996cacb5f433c
Wes Hendley <wrhendley@gmail.com>
2023-04-26 12:12:20 -0500

    Add incorrect location method

--- mastermind.rb
+++ mastermind.rb
@@ -24,7 +24,27 @@ class Board
     answer
   end
 
-  def check_incorrect_locations(code)
+  def check_incorrect_locations(guess)
+    count_for_color_guess = Hash.new(0)
+    count_for_color_code = Hash.new(0)
+    
+    guess.each do |color|
+      count_for_color_guess[color] += 1
+    end
+    
+    @code.each do |color|
+      count_for_color_code[color] += 1
+    end
 
+    guess.each_with_index do |color, index|
+      next if color == @code[index]
+
+      if count_for_color_guess[@code[index]] > count_for_color_code[@code[index]]
+        answer << 'w'
+        count_for_color_guess[@code[index]] -= 1
+      end
+    end
+    answer
+    end
   end
 end
